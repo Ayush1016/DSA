4317f48af7f48250bfc3490e8d2391691b82510f
Ayush Raj <97796452+Ayush1016@users.noreply.github.com>
2023-10-05 11:34:57 -0700

    03_Search Insert Position.cpp

---
+++ String/03_Search Insert Position.cpp
@@ -0,0 +1,23 @@
+Link: https://leetcode.com/problems/search-insert-position/description/
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int s=0;
+       int e=nums.size()-1;
+       int mid;
+       while(s<=e){
+       mid=(s+e)/2;
+           if(nums[mid]==target){
+               return mid;
+           }
+           else if(nums[mid]<target){
+               s=mid+1;
+           }
+           else{
+           e=mid-1;
+           }
+       } 
+       return s;
+    }
+};

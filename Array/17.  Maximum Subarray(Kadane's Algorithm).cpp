3cde4bde5847505e570c2966fe8df1c60d94d442
Ayush Raj <97796452+Ayush1016@users.noreply.github.com>
2023-12-26 09:04:19 -0800

    17.  Maximum Subarray (Kadane's Algorithm).cpp

--- Array/17.  Maximum Subarray(Kadane's Algorithm).cpp
+++ Array/17.  Maximum Subarray(Kadane's Algorithm).cpp
@@ -4,13 +4,13 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int sum=0;
-        int maxarr=nums[0];
+        int maxarr=nums[0];                 // Update the current sum by adding the current element
         for(int i=0;i<nums.size();i++){
                 sum=sum+nums[i];
-                maxarr=max(sum,maxarr);
+                maxarr=max(sum,maxarr);     // Update maxarr with the maximum of the current sum and maxarr
         if(sum<0){
-            sum=0;
-        }
+            sum=0;                // Reset sum to 0 if it becomes negative
+            }
         }
         return maxarr;
     }
